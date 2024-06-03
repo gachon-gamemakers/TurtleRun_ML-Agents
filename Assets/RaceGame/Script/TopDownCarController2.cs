873875ea81a6759afc2475abe2e05bf974cf4314
IR0NI <kla538538@gmail.com>
2024-06-03 21:01:39 +0900

    fix : turtlerun mlagents 버그픽스

---
+++ Assets/RaceGame/Script/TopDownCarController2.cs
@@ -0,0 +1,12 @@
+using Sirenix.OdinInspector;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace Gamandol.Race
+{
+    public class TopDownCarController2 : SerializedMonoBehaviour
+    {
+    }
+}
